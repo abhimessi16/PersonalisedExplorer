2f3709bf04f545e7da0553172be0fc1de42c10d2
abhimessi16 <91337858+abhimessi16@users.noreply.github.com>
2022-02-19 11:04:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,29 @@
 from pynput import keyboard
 import winsound,time
 
+# the frequency and duration of the beep when activation is done
 freq,dur=2500,500
 
+# the combo keys that will activate the program
+# i selected them so that they don't coincide with system hotkeys
 combo={keyboard.Key.ctrl_l, keyboard.Key.alt_l}
 
+# this will store all the keys that have been pressed
 curr=set()
 
+# to store the time at the start and end of key events
+# to be able to check the duration of the key press
 Start=0
 End=0
+
+# a flag to check if the activation-hotkey was pressed f=1
+# or if the activation-hotkey was released f=0
 f=0
+
+# c will store the keys that are pressed at any given instant of time
 c=set()
 
+# the code for the on_press event
 def on_press(key):
     global Start,End,f,work
     if key in combo:
@@ -24,6 +36,7 @@ def on_press(key):
     if key==keyboard.Key.esc:
         listener.stop()
 
+# the code for the on_release event
 def on_release(key):
     global Start,End,f,c,work
     try:
@@ -40,5 +53,6 @@ def on_release(key):
     except KeyError:
         pass
 
+# context manager for the keyboard listener
 with keyboard.Listener(on_press=on_press,on_release=on_release) as listener:
     listener.join()
